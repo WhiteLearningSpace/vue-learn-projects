158fbe4fbb836c4af141edb3d91018149bb4d726
LinWhite2333 <linwhite2333@outlook.com>
2024-04-10 15:27:28 +0800

    Update TodoAdd.spec.ts with new tests and assertions

--- vitest-test/src/components/TodoAdd.spec.ts
+++ vitest-test/src/components/TodoAdd.spec.ts
@@ -3,11 +3,10 @@ import { mount } from '@vue/test-utils'
 import TodoAdd from '@/components/TodoAdd.vue'
 
 describe('测试TodoAdd组件', () => {
-
   test('测试添加新的todo', async () => {
     const component = mount(TodoAdd)
-    // 设置input的值
     const inputElement = component.find('input')
+    // 设置input的值
     await inputElement.setValue('new todo')
     // 断言input的值
     expect(inputElement.element.value).toBe('new todo')
@@ -16,11 +15,21 @@ describe('测试TodoAdd组件', () => {
     // 检查是否触发了 addTodo 事件
     expect(component.emitted()).toHaveProperty('addTodo')
     // 检查事件的有效载荷
-    await inputElement.setValue('new todo1')
-    await component.find('form').trigger('submit')
-
     expect(component.emitted().addTodo[0]).toEqual(['new todo'])
-    expect(component.emitted().addTodo[1]).toEqual(['new todo1'])
   })
+
+  test('测试添加空的todo', async () => {
+    const component = mount(TodoAdd)
+    const inputElement = component.find('input')
+    // 设置input的值
+    await inputElement.setValue('')
+    // 断言input的值
+    expect(inputElement.element.value).toBe('')
+    // 触发submit事件
+    await component.find('form').trigger('submit')
+    // 检查是否触发了 addTodo 事件
+    expect(component.emitted()).not.toHaveProperty('addTodo')
+  })
+
 
 })
